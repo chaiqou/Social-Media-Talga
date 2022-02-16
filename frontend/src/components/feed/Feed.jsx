7c8082c5c609540a65207102ac301687e12ab581
chaiqou <LOMTADZENIKUSHA@gmail.com>
2022-02-16 23:38:28 +0400

    implement Hearts nice animated spinner for loading animation with react-loader-spinner

--- frontend/src/components/feed/Feed.jsx
+++ frontend/src/components/feed/Feed.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { client } from "../../client";
+import MasonryLayout from "../MasonryLayout/MasonryLayout";
+import Spinner from "../spinner/Spinner";
 
 const Feed = () => {
+  const [loading, setLoading] = useState(true);
+  if (loading)
+    return <Spinner message="We are looking for new photos for your feed <3" />;
+
   return <div>Feed</div>;
 };
 
