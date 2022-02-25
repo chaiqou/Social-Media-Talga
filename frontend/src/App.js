22c198ba5d13e16b3b339dfa847fe2f880e04b2d
chaiqou <lomtadzenikusha@gmail.com>
2022-02-26 00:01:32 +0400

    fix bug now if user exists load site , if not exists navigate to login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "./components/login/Login";
 import Home from "./components/home/Home";
 
 import { Routes, Route, useNavigate } from "react-router-dom";
 
 const App = () => {
+  const navigate = useNavigate();
+
+  // tu user iqneba wamoigebs users tu arada navigate to login
+
+  useEffect(() => {
+    const User =
+      localStorage.getItem("user") !== "undefined"
+        ? JSON.parse(localStorage.getItem("user"))
+        : localStorage.clear();
+
+    if (!User) navigate("/login");
+  }, []);
+
   return (
     <Routes>
       <Route path="login" element={<Login />} />
